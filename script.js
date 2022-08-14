40fa80eb19e2570878dee3076624a5a2e897a911
annony <ltn2057@protonmail.com>
2022-08-14 09:47:30 -0400

    Fix password validity check + fix responsiveness issues

--- script.js
+++ script.js
@@ -2,10 +2,9 @@ const form = document.querySelector('#sign-up')
 form.onsubmit = () => {
     const pass1 = document.querySelector('#password')
     const pass2 = document.querySelector('#confirm-pass')
-    if (pass1 !== pass2) {
+    if (pass1.value != pass2.value) {
         pass1.classList.add('error')
         pass2.classList.add('error')
-        console.log('pass')
     }
     else {
         pass1.classList.remove('error')
